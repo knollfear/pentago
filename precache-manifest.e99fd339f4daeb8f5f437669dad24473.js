a7dc58955b0da988f753f83645c890cf18827b11
Michael Knoll <mknoll@fearless.tech>
2019-06-25 23:15:18 -0400

    Updates

---
+++ precache-manifest.e99fd339f4daeb8f5f437669dad24473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1929f2522a19f264446dbb646e399ec",
+    "url": "/pentago/index.html"
+  },
+  {
+    "revision": "7b56ad08041bd16f6603",
+    "url": "/pentago/static/css/main.f8757eeb.chunk.css"
+  },
+  {
+    "revision": "dec20e4caaad8589701a",
+    "url": "/pentago/static/js/2.a7c477f3.chunk.js"
+  },
+  {
+    "revision": "7b56ad08041bd16f6603",
+    "url": "/pentago/static/js/main.e561e70d.chunk.js"
+  },
+  {
+    "revision": "da2199979de25733f942",
+    "url": "/pentago/static/js/runtime~main.3aaa5e29.js"
+  }
+]);
